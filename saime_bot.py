a301d69eb31a869564a38cf01de31cfb05fd93da
Wolfgang Salazar <wolfgangsalazarmora@hotmail.com>
2018-03-04 21:57:10 -0400

    Add more

--- saime_bot.py
+++ saime_bot.py
@@ -10,18 +10,32 @@ PASSWORD = config['DEFAULT']['PASSWORD']
 RECAPTCHA_TOKEN = ""
 
 LOGIN_URL = "https://tramites.saime.gob.ve/index.php?r=site/login"
-URL = "https://tramites.saime.gob.ve/index.php?r=tramite/tramite/"
+HOME_URL = "https://tramites.saime.gob.ve/index.php?r=tramite/tramite/"
 
 
 def check_login(session_requests):
-    result = session_requests.get(URL, headers = dict(referer = URL))
+    result = session_requests.get(HOME_URL, headers = dict(referer = HOME_URL))
     tree = html.fromstring(result.content)
-    print(result.content)
     try:
         login_form = tree.get_element_by_id('login-form')
         return False
     except KeyError:
         return True
+
+
+def get_user_data(session_requests):
+    result = session_requests.get(HOME_URL, headers = dict(referer = HOME_URL))
+    tree = html.fromstring(result.content)
+    lxml.
+    try:
+        login_form = tree.get_element_by_id('login-form')
+        return False
+    except KeyError:
+        return True
+
+def get_table_row(table_node):
+    return None
+
 
 
 def main():
